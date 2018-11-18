33dbf71477d783565147be09abc0d13b987b8d26
Jerry Marshall <jerrymarshall67@gmail.com>
2018-11-17 21:16:10 -0500

    forgot the >> on the last line

--- create-sh.sh
+++ create-sh.sh
@@ -3,5 +3,5 @@ read -p "Name of new script: "
 touch $REPLY
 echo "#!/bin/bash" >> $REPLY
 echo "# Script file created by Jerry Marshall" >> $REPLY
-echo "# Random text"
+echo "# Random text" >> $REPLY
 chmod +x $REPLY
